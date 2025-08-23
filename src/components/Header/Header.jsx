f7f54bbd4353efecc25f8ce3661c0904f50b710b
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-23 15:32:05 +0530

    create Header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-content">
+            <h2>Order Your Favourite Furniture Here</h2>
+            <p>Discover timeless furniture designed to make every corner of your home warm, stylish, and comfortable</p>
+            <button>View More...</button>
+        </div>
+
+    </div>
+    
+  )
+}
+
+export default Header
