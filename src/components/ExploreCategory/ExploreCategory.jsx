599bd6c0a6f71d7f801f75505afcf2462ec30dcf
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-08-30 11:16:09 +0530

    create explore categories section

---
+++ src/components/ExploreCategory/ExploreCategory.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect,useState } from 'react'
+import { getCategories } from '../../services/api.js';
+import './ExploreCategory.css';
+
+
+const ExploreCategory = () => {
+
+  //use state for backend data 
+  const [categories, setCategories] = React.useState([]);
+
+   //use state for loading data
+  const [loding, setLoading] = React.useState(true);
+
+   //use state for errors
+  const [error, setError] = React.useState(null);
+
+  useEffect(() => {
+
+    getCategories()
+    .then ((res) => {
+      console.log("API Response:", res.data);
+      setCategories(res.data.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error(error);
+      setError("Failed to fetch items");
+      setLoading(false);
+    });
+  }, []);
+
+  if (loding) return <p style={{ color: 'orange' }}>Loading...</p>;
+  if (error) return <p style={{ color: 'red' }}>{error}</p>;
+
+  return (
+    <div className="explore-category" id='explore-category'>
+      <h1>Explore Our Category </h1> <hr />
+      <p className="explore-category-text">Explore our diverse furniture categories, each offering thoughtfully designed pieces to transform your space into a home you love. </p>
+      <div className="explore-category-list">
+        {categories.map((category) => {
+          return(
+            <div key={category._id} className="explore-category-list-item">
+              <img src={`http://localhost:4000${category.categoryImage}`} alt={category.categoryName} />
+              <p>{category.categoryName}</p>
+            </div>
+          )
+
+        })}
+
+
+      </div>
+
+    </div>
+  );
+
+};
+
+export default ExploreCategory
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Home.css'
-import Header from '../../components/Header/Header'
+import Header from '../../components/Header/Header.jsx'
+import ExploreCategory from '../../components/ExploreCategory/ExploreCategory.jsx'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <ExploreCategory/>
+    
     </div>
   )
 }
